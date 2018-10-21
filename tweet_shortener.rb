732ed701f2d7ec4de0ff07d84ebb0c80ff1ebf4a
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 02:19:53 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,7 +21,7 @@ $dict = {
 
 def word_substituter(tweet)
   tweet.split().map{|word|
-    if $dict.has_key?(word) 
+    if $dict.has_key?(word)
       $dict[word]
     elsif $dict.has_key?(word[0].upcase+word[1...word.length])
       word[0].upcase+word[1...word.length]
