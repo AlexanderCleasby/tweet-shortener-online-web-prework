6d880049938c4b380df689124241f38b693029df
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 02:08:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,8 +25,7 @@ def word_substituter(tweet)
       $dict[word]
     else word
     end
-    
-}.join(" ")
+    }.join(" ")
 end
 
 #puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
