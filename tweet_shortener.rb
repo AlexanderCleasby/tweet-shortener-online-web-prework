c3c257968442721681b669550161baeabb4e01ed
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 02:07:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,4 +29,4 @@ def word_substituter(tweet)
 }.join(" ")
 end
 
-puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+#puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
