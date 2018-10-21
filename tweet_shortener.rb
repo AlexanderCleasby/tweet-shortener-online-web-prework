f866d9aa85b8ab7196ce1091884edab9bd3856c6
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 01:54:17 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,3 +2,5 @@
 def word_substituter(tweet)
   tweet.split()
 end
+
+word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
