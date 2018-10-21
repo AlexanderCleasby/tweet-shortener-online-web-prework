d0c82e7f80aa95d10c39f7e1bb1aad530755f046
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 01:55:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def word_substituter(tweet)
   tweet.split().map{|word|
-  word+"!"}
+  word+"!"}.join(" ")
 end
 
 puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
